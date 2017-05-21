b93f5c66436bbf055184caba21db2e44e387fe94
Silago Nevermind <silago.nevermind@gmail.com>
2017-05-21 15:42:51 +0300

    migrations

---
+++ base/migrations/0039_kvstore.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base','0038_delete_textdoubleimageitem')
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='KVStore',
+            fields=[
+                ('key', models.CharField(
+                    serialize=False,
+                    db_column='Key',
+                    max_length=200,
+                    primary_key=True)
+                    ),
+                ('value',models.TextField())
+                ]
+        ),
+    ]
